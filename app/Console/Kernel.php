7915e8aff6e9cf5942fe664d4e1bcf19e176560c
chiprodoy <chiprodoy@gmail.com>
2023-09-13 05:44:50 +0700

    kurangi jadwal syncron menjadi setiap menit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('moodle:syncgroup create')
-                ->everySixHours()->withoutOverlapping()
+                ->everyMinute()
+                ->withoutOverlapping()
                 ->emailOutputOnFailure('syazili@binadarma.ac.id');
         // $schedule->command('inspire')->hourly();
     }
